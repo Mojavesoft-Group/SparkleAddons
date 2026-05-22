756684c4afbbd6b60ce51572e97fe10cfa153ed5
SparkleBot <mail@example.com>
2026-05-22 17:22:10 +0000

    Format JavaScript code

--- mods/jameson-misc.js
+++ mods/jameson-misc.js
@@ -31,13 +31,14 @@ return class extends Mod {
 
     main() {
         this.api.disallowSnaps("Jameson");
-        this.api.ide.cloudMenu = function () {
-    var menu,
-        world = this.world(),
-        pos = this.controlBar.cloudButton.bottomLeft(),
-        shiftClicked = (world.currentKey === 16);
+        this.api.ide.cloudMenu = function() {
+            var menu,
+                world = this.world(),
+                pos = this.controlBar.cloudButton.bottomLeft(),
+                shiftClicked = (world.currentKey === 16);
 
-        this.inform("The Jameson Project", "Cloud functions are not currently supported.")}
+            this.inform("The Jameson Project", "Cloud functions are not currently supported.")
+        }
     };
 
     cleanupFunc() {
