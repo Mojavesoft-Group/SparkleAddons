d8a29bf5f61f5adb56702dbe7cd926d9e24fe213
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-03 13:08:03 -0400

    Update jameson-logo.js to bump version number

--- mods/jameson-logo.js
+++ mods/jameson-logo.js
@@ -24,7 +24,7 @@ return class extends Mod {
     ID = "jameson-logo";
     NAME = "Jameson Logo";
     DESCRIPTION = "Replace the Snap! logo with Jameson's logo.";
-    VERSION = "1.2.1";
+    VERSION = "1.3.0";
     AUTHOR = "PPPDUD";
     DEPENDS = [];
     DO_MENU = false;
