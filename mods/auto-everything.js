3aad58a52edf1d570fc51265b80105c71feb36d8
SparkleBot <mail@example.com>
2026-06-04 19:48:41 +0000

    Format JavaScript code

--- mods/auto-everything.js
+++ mods/auto-everything.js
@@ -35,23 +35,24 @@ return class extends Mod {
 
     main() {
         let myself = this;
+
         function update_settings() {
             if (myself.options.dropDown == "Yes") {
-        SpriteMorph.prototype.showingExtensions =
-            true;
-        myself.api.ide.flushBlocksCache('variables');
-        myself.api.ide.refreshPalette();
-        myself.api.ide.categories.refreshEmpty();
-        }
-    
-        if (myself.options.dropDown2 == "Yes") {
-        Process.prototype.enableJS = true;
-        }
+                SpriteMorph.prototype.showingExtensions =
+                    true;
+                myself.api.ide.flushBlocksCache('variables');
+                myself.api.ide.refreshPalette();
+                myself.api.ide.categories.refreshEmpty();
+            }
+
+            if (myself.options.dropDown2 == "Yes") {
+                Process.prototype.enableJS = true;
+            }
         }
         update_settings();
 
         myself.addEventListener("optionsChanged", () => {
-                myself.api.ide.inform("Auto Everything", "In order for your changes to take effect, you need to reload Snap!.");
+            myself.api.ide.inform("Auto Everything", "In order for your changes to take effect, you need to reload Snap!.");
         });
     }
 
